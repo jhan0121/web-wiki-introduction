a86a698ecf89c567d0c5e6b0bb4e4b4f155d0987
jhan0121 <jhan0121@gmail.com>
2025-02-27 13:19:29 +0900

    feat: 댓글 추가 기능 구현

--- js/index.js
+++ js/index.js
@@ -16,4 +16,50 @@ console.log(
   "color: #d81b60; font-size: 16px; font-weight: bold;"
 );
 
-console.log("알맞은 스크립트를 작성하세요");
+function addComment() {
+  const textComment = document.getElementById("comment-writing").value;
+  const li = document.createElement("li");
+
+  const commentItem = document.createElement("div");
+  commentItem.className = "comment-item";
+
+  const commentAuthor = createAuthor();
+  const commentContent = createCommentContent(textComment);
+
+  commentItem.appendChild(commentAuthor);
+  commentItem.appendChild(commentContent);
+  li.appendChild(commentItem);
+  document.querySelector(".comment-list").appendChild(li);
+  document.getElementById("comment-writing").value = "";
+}
+
+function createImage() {
+  const img = document.createElement("img");
+  img.src = "./images/comment-author-icon.png";
+  img.alt = "사용자 프로필 이미지";
+  return img;
+}
+
+function createSpan() {
+  const span = document.createElement("span");
+  span.textContent = "방문자";
+  return span;
+}
+
+function createCommentContent(textComment) {
+  const commentContent = document.createElement("div");
+  commentContent.className = "comment-content";
+  commentContent.textContent = textComment;
+  return commentContent;
+}
+
+function createAuthor() {
+  const commentAuthor = document.createElement("div");
+  commentAuthor.className = "comment-author";
+
+  const img = createImage();
+  const span = createSpan();
+  commentAuthor.appendChild(img);
+  commentAuthor.appendChild(span);
+  return commentAuthor;
+}
